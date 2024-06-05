537ba0ad18177655dadf5a5d67c37b604e4328a4
JosiahCPP <jtwiggins@cpp.edu>
2024-06-05 14:08:16 -0700

    Update buff.v

--- Group J/Lab0/buff.v
+++ Group J/Lab0/buff.v
@@ -1,7 +1,28 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 06/03/2024 10:53:40 PM
+// Design Name: 
+// Module Name: buff
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
 module buff(
     input [15:0] SW,
     output [15:0] LED
     );
-
     assign LED = SW;
 endmodule

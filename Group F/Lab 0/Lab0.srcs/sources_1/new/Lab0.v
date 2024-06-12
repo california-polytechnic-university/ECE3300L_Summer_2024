f66c1372c66f02661c9a92f843ab2cc7c67bf71c
Jci2002 <73627301+Jci2002@users.noreply.github.com>
2024-06-12 12:28:09 -0700

    Add files via upload

---
+++ Group F/Lab 0/Lab0.srcs/sources_1/new/Lab0.v
@@ -0,0 +1,30 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 06/10/2024 09:30:49 PM
+// Design Name: 
+// Module Name: Lab0
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Lab0(
+    input [15:0] SW,
+    output [15:0] LED
+    );
+    
+    assign LED = SW;
+    
+endmodule

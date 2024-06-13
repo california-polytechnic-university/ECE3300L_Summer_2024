d06cad394fba87103307b0357fc216952879226f
JosiahCPP <jtwiggins@cpp.edu>
2024-06-13 00:10:45 -0700

    Update mux_2x1.v

--- Group J/Lab2/mux_2x1.v
+++ Group J/Lab2/mux_2x1.v
@@ -1,1 +1,35 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 06/12/2024 09:10:24 PM
+// Design Name: 
+// Module Name: mux_2x1
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module mux_2x1(
+    input x1, x2, s,
+    output f
+    );
+    wire sn,g0,g1;
+    
+    not (sn, s);
+    and A0 (g0, x1, sn);
+    and A1 (g1, x2, s);
+    or O0 (f, g0, g1);
+    
+endmodule
 

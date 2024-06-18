a817f1ca5764eb9828da60fdd995925c61af35bd
nagomez7190 <143862423+nagomez7190@users.noreply.github.com>
2024-06-18 08:01:07 -0700

    Update mux4x1.v

--- Group B/Lab 2/mux4x1.v
+++ Group B/Lab 2/mux4x1.v
@@ -9,8 +9,8 @@ module mux4x1(
     not (tmp[5], sel[0]);
     not (tmp[4], sel[1]);
     and (tmp[3], tmp[5], tmp[4], mux_inp[0]);
-    and (tmp[2], tmp[5], sel[1], mux_inp[1]);
-    and (tmp[1], sel[0], tmp[4], mux_inp[2]);
+    and (tmp[2], tmp[4], sel[0], mux_inp[1]);
+    and (tmp[1], sel[1], tmp[5], mux_inp[2]);
     and (tmp[0], sel[0], sel[1], mux_inp[3]);
     or (mux_out, tmp[3], tmp[2], tmp[1], tmp[0]); 
     

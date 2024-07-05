8075142955182c82e3fbf740a2ef99eb6145a7ae
Ethan McKendell <145165919+emmckendell@users.noreply.github.com>
2024-07-04 19:06:59 -0700

    Update clk_up_counter.v

--- Group L/Lab3/clk_up_counter.v
+++ Group L/Lab3/clk_up_counter.v
@@ -32,47 +32,50 @@ module clk_up_counter(
     reg [31:0] clk_count;
     reg [31:0] max_count;
 
-    always @(*) begin
+    always @(clk_frequency) 
+    begin
         case (clk_frequency)
             
             // clk = 100Mhz = 1 0000 0000Hz (1 0000 0000 cycles/second)
             // the clk is slowed down using an up_counter to waste cycles
-            // if the up_counter is counting up to 5000 0000 it will take ~0.5 seconds to complete this action
+            // if the up_counter is counting up to 5000 0000 it will take 0.5 seconds to complete this action
+            // every 0.5 seconds the clk_divider = ~clk_divider
             // using this information, the posedge of the clk will occur every second
-            
-            5'b00000 : max_count = 1000000000; // every 2 seconds 7seg updates
-            5'b00001 : max_count = 50000000;  // every second 7seg updates
-            5'b00010 : max_count = 40000000;
-            5'b00011 : max_count = 30000000;
-            5'b00100 : max_count = 20000000;
-            5'b00101 : max_count = 10000000;
-            5'b00110 : max_count = 9000000;
-            5'b00111 : max_count = 8000000;
-            5'b01000 : max_count = 7000000;
-            5'b01001 : max_count = 6000000;
-            5'b01010 : max_count = 5000000;
-            5'b01011 : max_count = 4000000;
-            5'b01100 : max_count = 3000000;
-            5'b01101 : max_count = 2000000;
-            5'b01110 : max_count = 1000000;
-            5'b01111 : max_count = 900000;
-            5'b10000 : max_count = 800000;
-            5'b10001 : max_count = 700000;
-            5'b10010 : max_count = 600000;
-            5'b10011 : max_count = 500000;
-            5'b10100 : max_count = 400000;
-            5'b10101 : max_count = 300000;
-            5'b10110 : max_count = 200000;
-            5'b10111 : max_count = 100000; // every 2 milliseconds 7seg updates
-            5'b11000 : max_count = 90000;
-            5'b11001 : max_count = 80000;
-            5'b11010 : max_count = 70000;
-            5'b11011 : max_count = 60000;
-            5'b11100 : max_count = 50000; // every millisecond 7seg updates
-            5'b11101 : max_count = 40000;
-            5'd11110 : max_count = 30000;
-            5'd11111 : max_count = 20000;
-            default: max_count = 10000;
+            // max_count should not exceed 42 9496 7295 (32-bit integer limit)
+                       
+            5'b00000 : max_count = 32'd1000000000; // every 10 seconds 7seg updates
+            5'b00001 : max_count = 32'd500000000;  // every 5 seconds 7seg updates
+            5'b00010 : max_count = 32'd200000000;  // every 3 seconds 7seg updates
+            5'b00011 : max_count = 32'd100000000;  // every 2 seconds 7seg updates
+            5'b00100 : max_count = 32'd50000000;   // every 1 second 7seg updates
+            5'b00101 : max_count = 32'd40000000;
+            5'b00110 : max_count = 32'd30000000;
+            5'b00111 : max_count = 32'd20000000;
+            5'b01000 : max_count = 32'd10000000;
+            5'b01001 : max_count = 32'd9000000;
+            5'b01010 : max_count = 32'd8000000;
+            5'b01011 : max_count = 32'd7000000;
+            5'b01100 : max_count = 32'd6000000;
+            5'b01101 : max_count = 32'd5000000;
+            5'b01110 : max_count = 32'd4000000;
+            5'b01111 : max_count = 32'd3000000;
+            5'b10000 : max_count = 32'd2000000;
+            5'b10001 : max_count = 32'd1000000;
+            5'b10010 : max_count = 32'd900000;
+            5'b10011 : max_count = 32'd800000;
+            5'b10100 : max_count = 32'd700000;
+            5'b10101 : max_count = 32'd600000;
+            5'b10110 : max_count = 32'd500000;
+            5'b10111 : max_count = 32'd400000;
+            5'b11000 : max_count = 32'd300000;
+            5'b11001 : max_count = 32'd200000;
+            5'b11010 : max_count = 32'd100000;     // every 2 milliseconds 7seg updates
+            5'b11011 : max_count = 32'd90000;
+            5'b11100 : max_count = 32'd80000;
+            5'b11101 : max_count = 32'd70000;
+            5'b11110 : max_count = 32'd60000;
+            5'b11111 : max_count = 32'd50000;      // every millisecond 7seg updates
+            default: max_count = 32'd3000000000;   // every minute 7seg updates
         endcase
     end
 
@@ -80,18 +83,23 @@ module clk_up_counter(
     begin
         if(rst) 
             begin
-                clk_count <= 0;
+                // rst button will reset counter and clk_divider
+                // if held the clk_divider will continue to be LOW
+                // meaning there is no posedge clk so the 7seg will not count up
+                
+                clk_count <= 32'd0;
                 clk_divider <= 1'b0;
             end 
             else 
-            begin
-                if(clk_count >= max_count) begin
-                    clk_count <= 0;  // reset counter to 0 when max_count is reached
-                    clk_divider <= ~clk_divider;
-            end 
+                begin
+                if(clk_count >= max_count) 
+                    begin
+                        clk_count <= 32'd0;  // reset counter to 0 when max_count is reached
+                        clk_divider <= ~clk_divider;
+                    end 
             else 
                 begin
-                clk_count <= clk_count + 1;
+                    clk_count <= clk_count + 32'd1;
                 end
         end
     end

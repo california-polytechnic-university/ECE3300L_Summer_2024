21672a48491a60734929579548d94ab10d3a46f7
cvduong <cvduong@cpp.edu>
2024-07-09 23:28:39 -0700

    Update top.v

--- Group A/Lab4/top.v
+++ Group A/Lab4/top.v
@@ -1,5 +1,25 @@
 `timescale 1ns / 1ps
 
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 07/07/2024 04:34:38 PM
+// Design Name: 
+// Module Name: top
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
 module top(
     input clk,                
     input reset,              

7725b96ca4e3598f64bed582473e72c045d0dead
cvduong <cvduong@cpp.edu>
2024-07-09 23:29:03 -0700

    Update hex2sseg.v

--- Group A/Lab4/hex2sseg.v
+++ Group A/Lab4/hex2sseg.v
@@ -1,5 +1,25 @@
 `timescale 1ns / 1ps
 
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 07/07/2024 04:34:11 PM
+// Design Name: 
+// Module Name: hex2sseg
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
 module hex2sseg(
     input [3:0] hex,
     output reg [6:0] sseg

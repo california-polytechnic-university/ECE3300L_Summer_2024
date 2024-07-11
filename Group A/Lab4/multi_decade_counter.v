2b752b9ac669662cd7ac6142f05b77098f994174
William-Masri <122050197+William-Masri@users.noreply.github.com>
2024-07-10 22:04:53 -0700

    Update multi_decade_counter.v

--- Group A/Lab4/multi_decade_counter.v
+++ Group A/Lab4/multi_decade_counter.v
@@ -1,12 +1,33 @@
 `timescale 1ns / 1ps
 
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 07/08/2024 05:55:03 PM
+// Design Name: 
+// Module Name: multi_decade_counter
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
 module multi_decade_counter(
+
     input clk,
     input reset,
     input enable,
-    input type, 
-    input updown,
-    output reg eout,
+    input mode, 
+    input direction,
+    output reg enable_out,
     output reg [3:0] count
     );
     
@@ -16,24 +37,24 @@ module multi_decade_counter(
             count <= 0;
         else if(enable)
         begin
-                if(!type && !updown)
+                if(!mode && !direction) // bcd and up
                 begin
-                    eout <= (count == 8);
+                    enable_out <= (count == 8);
                     count <= (count == 9) ? 4'b0000 : count + 1;
                 end
-                else if(type && !updown)
+                else if(mode && !direction) // hex and up\
                 begin
-                     eout <= (count == 14);
+                     enable_out <= (count == 14);
                      count <= (count == 15) ? 4'b0000 : count + 1;
                 end
-                else if(!type && updown)
+                else if(!mode && direction) // bcd and down
                 begin
-                     eout <= (count == 1);
+                     enable_out <= (count == 1);
                      count <= (count == 0) ? 4'b1001 : count - 1;
                 end
                 else
                 begin
-                     eout <= (count == 1);
+                     enable_out <= (count == 1);
                      count <= (count == 0) ? 4'b1111 : count - 1;
                 end
         end

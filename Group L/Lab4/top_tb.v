81f11d02c156acff2dd53820a12865eb0af43da7
lrzman57 <97260394+lrzman57@users.noreply.github.com>
2024-07-11 22:11:33 -0700

    Update top_tb.v

--- Group L/Lab4/top_tb.v
+++ Group L/Lab4/top_tb.v
@@ -1,1 +1,59 @@
 
+module tb_top;
+  reg clk;
+  reg rst;
+  reg en;
+  reg mode;
+  reg direction;
+  reg [4:0] clk_frequency;
+  wire [7:0] AN;
+  wire [6:0] SEG;
+
+
+  top uut (
+    .clk(clk),
+    .rst(rst),
+    .en(en),
+    .mode(mode),
+    .direction(direction),
+    .clk_frequency(clk_frequency),
+    .AN(AN),
+    .SEG(SEG)
+  );
+
+
+  initial clk = 0;
+  always #5 clk = ~clk;  
+
+ 
+  initial begin
+
+    rst = 1;
+    en = 0;
+    mode = 0;
+    direction = 1;
+    clk_frequency = 5'b00100;  
+
+
+    #100;
+    rst = 0;
+    
+
+    #100;
+    en = 1;
+
+
+    #1000;
+    mode = 1;  
+    #1000;
+    direction = 0;  
+    #1000;
+    mode = 0;  
+    #1000;
+    direction = 1;  
+
+    
+    #10000;
+    $finish;
+  end
+endmodule

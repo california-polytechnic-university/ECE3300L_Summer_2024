419ad9eb15f3e5ec26a6110893c8e7fe624a5227
ryantran373 <91349480+ryantran373@users.noreply.github.com>
2024-07-15 11:30:19 -0700

    Create up_down_counter.v

---
+++ Group E/Lab 4/up_down_counter.v
@@ -0,0 +1,34 @@
+module up_down_counter(
+    input clk, rst, en, sel, updown,
+    output reg [3:0] digit,
+    output reg carry
+);
+    always@(posedge clk or posedge rst)
+    begin
+        if(rst)
+            digit <= 0;
+        else if(en)
+        begin
+                if(sel && updown) // BCD up logic
+                begin
+                    carry <= (digit == 8);
+                    digit <= (digit == 9) ? 4'b0000 : digit + 1;
+                end
+                else if(!sel && updown) // Hex up logic
+                begin
+                     carry <= (digit == 14);
+                     digit <= (digit == 15) ? 4'b0000 : digit + 1;
+                end
+                else if(sel && !updown) // BCD down logic
+                begin
+                     carry <= (digit == 1);
+                     digit <= (digit == 0) ? 4'b1001 : digit - 1;
+                end
+                else // Hex down logic
+                begin
+                     carry <= (digit == 1);
+                     digit <= (digit == 0) ? 4'b1111 : digit - 1;
+                end
+        end
+    end
+endmodule

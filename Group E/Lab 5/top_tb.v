f7060fd62d42b9a8385d13cc0f132a6f79fe93b0
ryantran373 <91349480+ryantran373@users.noreply.github.com>
2024-07-22 13:04:15 -0700

    Update top_tb.v

--- Group E/Lab 5/top_tb.v
+++ Group E/Lab 5/top_tb.v
@@ -15,3 +15,78 @@ module top_tb();
         .load_button(load_button),
         .load(load)
     );
+        
+    load_counter lc1_inst(
+        .clk(clk),
+        .rst(rst),
+        .en(carry0),
+        .sel(sel),
+        .updown(updown),
+        .digit(digit1),
+        .carry(carry1),
+        .load_button(load_button),
+        .load(load)
+    );
+        
+    load_counter lc2_inst(
+        .clk(clk),
+        .rst(rst),
+        .en(carry1 & carry0),
+        .sel(sel),
+        .updown(updown),
+        .digit(digit2),
+        .carry(carry2),
+        .load_button(load_button),
+        .load(load)
+    );
+        
+    load_counter lc3_inst(
+        .clk(clk),
+        .rst(rst),
+        .en(carry2 & carry1 & carry0),
+        .sel(sel),
+        .updown(updown),
+        .digit(digit3),
+        .carry(),
+        .load_button(load_button),
+        .load(load)
+    );
+    
+    initial
+        begin
+        clk = 0;
+        rst = 1;
+        updown = 1;
+        sel = 1;
+        en = 0;
+        load_button = 0;
+        load = 4'b0000;
+        
+        #10 rst = 0;
+        #10 en = 1;
+        
+        #2000;
+        #10 updown = 0;
+        #10 sel = 0;
+        #2000;
+        
+        #10 en = 0;
+        #10 rst = 1;
+        #10 rst = 0;
+        #10 load = 4'b1111;
+        #10 load_button = 1;
+        #10 load_button = 0;
+        
+        #100;
+        #10 en = 1;
+        #100;        
+        #10 en = 0;
+        #10 load = 4'b0000;
+        #10 rst = 1;
+        #50;        
+        
+        $finish;
+        end
+        
+        always #5 clk = ~clk;
+endmodule

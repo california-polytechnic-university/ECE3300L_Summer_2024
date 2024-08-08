410cc4ffc8d520247df508b7e83721e16e206f58
William-Masri <122050197+William-Masri@users.noreply.github.com>
2024-08-07 20:11:42 -0700

    Create top.v

---
+++ Group D/Lab06/top.v
@@ -0,0 +1,32 @@
+`timescale 1ns / 1ps
+module top(
+    input clk,
+    input reset,
+    input [4:0] x, 
+    input [4:0] y, 
+    input [1:0] op, 
+    output [6:0] sseg,
+    output [7:0] AN 
+    );
+    wire [7:0] result;
+    wire sign;
+    bcd_calculator calc(
+        .reset(reset),
+        .x(x),
+        .y(y),
+        .op(op),
+        .result(result),
+        .sign(sign)
+    ); 
+    sseg_driver sd(
+        .clk(clk),
+        .reset(reset),
+        .x(x),
+        .y(y),
+        .op(op),
+        .result(result),
+        .sign(sign),
+        .sseg(sseg),
+        .AN(AN)
+    );
+endmodule
